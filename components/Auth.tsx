0e26bb26c0080188b66e7a2ce0c1f550fb85cf32
sarim2000 <sarimbleedblue@gmail.com>
2023-01-08 22:36:05 +0530

    upd

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -26,11 +26,11 @@ export default function Home() {
   async function login() {
     if (!sdkRef.current) {
       const socialLoginSDK = new SocialLogin()
-      const signature1 = await socialLoginSDK.whitelistUrl('https://biconomy-social-auth.vercel.app')
+      const signature1 = await socialLoginSDK.whitelistUrl('https://biconomy-app.vercel.app/')
       await socialLoginSDK.init({
         chainId: ethers.utils.hexValue(ChainId.POLYGON_MAINNET),
         whitelistUrls: {
-          'https://biconomy-social-auth.vercel.app': signature1,
+          'https://biconomy-app.vercel.app/': signature1,
         }
       })
       sdkRef.current = socialLoginSDK
